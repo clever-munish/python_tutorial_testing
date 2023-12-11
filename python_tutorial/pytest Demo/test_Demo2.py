6e0ec0e1628413aa9773196d906af87399a7c73a
clever-munish <151876455+clever-munish@users.noreply.github.com>
2023-12-12 02:24:28 +0530

    pytest tutorial

---
+++ python_tutorial/pytest Demo/test_Demo2.py
@@ -0,0 +1,17 @@
+# Generating JunitXML Report:
+# cmd pytest -rA --junit-xml="JunitxmlReport.xml"
+
+# Generating Html file
+# pip install pytest-html
+# pytes -rA --html="HTMLPyTestReport.html"
+
+def test_simple_one():
+    print("Inside the simple one")
+
+
+def test_simple_two():
+    print("Inside the simple two")
+
+
+def test_simple_three():
+    print("Inside the simple three")
